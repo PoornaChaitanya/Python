57b0a9eef6a6f8900cdec55e57772492f31ba57b
PoornaChaitanya <poornachaitanya4259@gmail.com>
2024-04-09 12:33:57 +0530

    code

---
+++ Half_Diamond_Pattern.py
@@ -0,0 +1,19 @@
+""""
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+"""
+
+n=int(input())
+for i in range(n):
+    print("*"*i)
+for j in range(n):
+    print("*"*(n-j))
---
+++ Handshakes.py
@@ -0,0 +1,4 @@
+#Number of handshakes happen for given number of persons
+
+n=int(input())
+print((n*(n-1))//2)
